9df1161683b7f87c8eb9a138d11325660ffe5135
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-10-31 21:43:34 +0200

    Adding a blank solution and a stub library. The solution is in the new *.slnx format.

---
+++ TteLcl.Reflections/AsmReflection.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * (c) 2025  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TteLcl.Reflections;
+
+/// <summary>
+/// Low level reflection utilities on .NET assemblies
+/// </summary>
+public class AsmReflection
+{
+  /// <summary>
+  /// Create a new AsmReflection
+  /// </summary>
+  public AsmReflection()
+  {
+  }
+
+}
